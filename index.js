b4cd0514ab11135915d67c2f5af6353941a226d3
Surenderdubeyofficial <surenderdubey221@gmail.com>
2024-10-13 09:09:00 +0530

    update

--- index.js
+++ index.js
@@ -1,23 +1,22 @@
+require('dotenv').config();
 const express = require('express');
 
 const app = express();
 
-app.get('/',(req,res)=>{
-    res.send("Welcome to advance backend js ");
-    
-})
-app.get('/twiter',(req,res)=>{
-    res.send(" hi twiter Welcome to advance backend js ");
-    
-})
-app.get('/login',(req,res)=>{
-    res.send(" hi twiter Welcome to advance backend js login section ");
-    
-})
+app.get('/', (req, res) => {
+    res.send("Welcome to advanced backend JS");
+});
 
-const port = 4000;
+app.get('/twitter', (req, res) => {
+    res.send("Hi Twitter, Welcome to advanced backend JS");
+});
 
-app.listen(port,()=>{
-    console.log(`Server is listeniing ${port}`);
-    
-})
+app.get('/login', (req, res) => {
+    res.send("Hi, Welcome to advanced backend JS login section");
+});  
+
+const port = process.env.PORT || 4000; // It will use 3000 from .env or default to 4000
+
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
+});
